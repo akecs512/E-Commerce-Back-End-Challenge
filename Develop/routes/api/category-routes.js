dd09596abf261853441663559c6ba1bad23ccc81
Adrienne Kecskes <akecs619@yahoo.com>
2023-08-19 15:54:13 -0500

    add category by id with product

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,7 +4,9 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/',async (req, res) =>  {
-  const categories = await Category.findAll()
+  const categories = await Category.findAll(
+    {include: Product}
+    )
   console.log(categories);
   res.status(200)
   res.send(JSON.stringify(categories));
@@ -13,7 +15,13 @@ router.get('/',async (req, res) =>  {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  console.log(req.params);
+  
+  const category = await Category.findOne({ where: { id: req.params.id },include: Product }, );
+  res.status(200)
+  res.send(JSON.stringify(category));
+  
   // find one category by its `id` value
   // be sure to include its associated Products
 });
